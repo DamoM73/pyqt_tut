8c638c3adc4ec43032494c5402b0b58f0fed8ae1
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2021-11-25 09:55:51 +1000

    stuff

---
+++ main.py
@@ -0,0 +1,12 @@
+from interface import Ui_Dialog
+
+ui = Ui_Dialog()
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec())
